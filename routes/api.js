4cb5f3c16b1973e25c501055f57cdbe955c419d5
Aayan <slmngoyo2004@gmail.com>
2024-07-02 20:56:23 +0300

    codes added

--- routes/api.js
+++ routes/api.js
@@ -1,33 +1,54 @@
 const express = require('express');
 const router = express.Router();
 const dns = require('dns');
+const bodyParser = require('body-parser');
+
 
 
 const verifiedUrl = {};
-router.post('/shorturl', (req, res, next) => {
-    const urlData = req.body.url;
-    const urlHostname = new URL(urlData).hostname;
+router.post('/:shorturl', (req, res) => {
+   const urlData = req.body.url;
 
-    dns.lookup(urlHostname, (error, address, family) => {
-        if (error){
+   console.log('urlData: ' + urlData);
+   
+ 
+   const options = {
+    all: true,
+   };
+
+
+    dns.lookup(urlData, options, (error, address, family) => {
+      if (error){
             if (error.code ===  'ENOTFOUND'){
-               return res.json({ error: 'Invalid url'});
+               return res.json({ error: 'Invalid url yes'});
+               
             } else
                return res.json({ error: 'DNS lookup error'});
+              
         } else {
             const id = Math.floor(Math.random() * 100);
             verifiedUrl[id] = urlData;
-            res.json({ 'original_url': urlData, 'short_url': id});
+            res.json({ 
+              'original_url': urlData,
+               'short_url': id
+              });
+            // return res.send({ 'original_url': urlData});
+            
         }
     })
+    
+    
 });
 
 
-router.get('/id', (req, res) => {
+router.get('/:shorturl/:id', (req, res) => {
     const shortUrlId = req.params.id;
+   
 
-    if(!shortUrlId || !verifiedUrl.hasOwnProperty(shortUrl)){
+    if(!shortUrlId || !verifiedUrl.hasOwnProperty(shortUrlId)){
+       // console.log('verifiedUrl: '+  verifiedUrl[shortUrlId]);
       return res.json({error: 'Short Url not Found'});
+    
     } else {
       res.redirect(verifiedUrl[shortUrlId]);
     } 
--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.json());
+
 
 
 const urlShortener = require('./routes/api');
 app.use('/api', urlShortener);
 
-const PORT = 3000;
+const PORT = 3000; 
 
 app.listen(PORT, () => {
     console.log(`Server is Running on PORT: ${PORT}`);
