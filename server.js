9616be137903a1b0d2196d19e84d1b3bda5761ec
Aayan <slmngoyo2004@gmail.com>
2024-07-13 15:46:07 +0300

    small changes

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-//const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const urlShortener = require('./routes/api');
 
-//app.use(bodyParser.json({ type: 'application/*+json' }))
-//app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/*+json' }))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
